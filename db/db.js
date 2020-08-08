fdd1b3930692b18708704937686ad258d98575cf
灰太狼 <765508681@qq.com>
2020-08-08 23:42:23 +0800

    finish

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql")
 const conn = mysql.createConnection({
-  host:"localhost",//数据库的地址
-  user:"root",
-  password:"1234",
-  database:"pdd"
+  host:"120.78.179.179",//数据库的地址
+  user:"pdd-api",
+  password:"123456",
+  database:"pdd-api"
 })
 conn.connect()
 module.exports = conn
